423a00721aca4116484d4a1b1a5237c1e5ea8700
Aishwarya <aishasadhwani@gmail.com>
2020-05-25 22:43:06 +0530

    final commit

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from blogapp.models import PostBlog
 from blogapp.forms import SignUpForm,PostBlogForm
+from django.urls import reverse
 from django.core.mail import send_mail
 from django.conf import settings
 from django.views.generic import (
 ListView,
-DetailView
+DetailView,
+CreateView,
+UpdateView,
+DeleteView,
+TemplateView, View
 )
 
 # Create your views here.
@@ -18,24 +24,24 @@ def home(request):
     return render(request,'blogapp/home.html')
 
 
-@login_required
-def postblog(request):
-    postblogform = PostBlogForm()
-    mydict = {'postblogform':postblogform}
-    if request.method == 'POST':
-        postblogform = PostBlogForm(request.POST,request.FILES)
-        if postblogform.is_valid():
-            data = postblogform.save()
-            data.author = str(request.user)
-            data.save()
-            mydict.update({'msg':'Posted Successfully !'})
-    return render(request,'blogapp/postblog.html',context=mydict)
+# @login_required
+# def postblog(request):
+#     postblogform = PostBlogForm()
+#     mydict = {'postblogform':postblogform}
+#     if request.method == 'POST':
+#         postblogform = PostBlogForm(request.POST,request.FILES)
+#         if postblogform.is_valid():
+#             data = postblogform.save()
+#             data.author = str(request.user)
+#             data.save()
+#             mydict.update({'msg':'Posted Successfully !'})
+#     return render(request,'blogapp/postblog.html',context=mydict)
 
 
-def viewblog(request):
-    blogs = PostBlog.objects.all().order_by('-posted_date')
-    mydict = {'blogs':blogs}
-    return render(request,'blogapp/viewblog.html',context=mydict)
+# def viewblog(request):
+#     blogs = PostBlog.objects.all().order_by('-posted_date')
+#     mydict = {'blogs':blogs}
+#     return render(request,'blogapp/viewblog.html',context=mydict)
 
 def SignupPage(request):
     signupform = SignUpForm()
@@ -56,37 +62,63 @@ def SignupPage(request):
             mydict.update({'msg':'User is registered successfully !'})
     return render (request,'blogapp/signup.html',context=mydict)
 
-@login_required
-def PostDetailView(request,pid):
-    blog = PostBlog.objects.get(id=pid)
-    return render(request,'blogapp/detailblog.html',{'blog':blog})
+# @login_required
+# def PostDetailView(request,pid):
+#     blog = PostBlog.objects.get(id=pid)
+#     return render(request,'blogapp/detailblog.html',{'blog':blog})
 
-@login_required
-def delete_new(request,pid):
-    context={}
-    blog = PostBlog.objects.get(id=pid)
+# @login_required
+# def delete_new(request,pid):
+#     context={}
+#     blog = PostBlog.objects.get(id=pid)
+#
+#     if request.method =="POST":
+#         # delete object
+#         blog.delete()
+#         # after deleting redirect to
+#         # home page
+#         return HttpResponseRedirect("/viewblog/")
+#     # return render(request,'blogapp/detailblog.html',{'blog':blog})
+#     return render(request, "blogapp/delete_view.html", context)
 
-    if request.method =="POST":
-        # delete object
-        blog.delete()
-        # after deleting redirect to
-        # home page
-        return HttpResponseRedirect("/viewblog/") 
-    # return render(request,'blogapp/detailblog.html',{'blog':blog})
-    return render(request, "blogapp/delete_view.html", context)
 
-# class PostListView(ListView):
-#     model = PostBlog
-#     template_name= 'blogapp/viewblog.html'
-#     context_object_name = 'blogs'
-#     ordering = ['-posted_date']
-#
-#
-# class PostDetailView(DetailView):
-#     model = PostBlog
-#     template_name='blogapp/detailblog.html'
-#
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         return context
+
+class PostList(ListView):
+    model = PostBlog
+    template_name= 'blogapp/viewblog.html'
+    context_object_name = 'blogs'
+    ordering = ['-posted_date']
+
+
+@method_decorator(login_required, name='dispatch')
+class CreateBlog(CreateView):
+    model = PostBlog
+    fields = '__all__'
+    template_name = 'blogapp/postblog.html'
+
+    def get_success_url(self):
+        return reverse('home')
+
+
+@method_decorator(login_required, name='dispatch')
+class DetailBlog(DetailView):
+    model = PostBlog
+    template_name = 'blogapp/detailblog.html'
+    context_object_name = 'blog'
+
+@method_decorator(login_required, name='dispatch')
+class UpdateBlog(UpdateView):
+    model = PostBlog
+    fields = '__all__'
+    template_name = 'blogapp/postblog.html'
+
+    def get_success_url(self):
+        return reverse('home')
+
+@method_decorator(login_required, name='dispatch')
+class DeleteBlog(DeleteView):
+    model = PostBlog
+    template_name = 'blogapp/delete_view.html'
+
+    def get_success_url(self):
+        return reverse('home')
